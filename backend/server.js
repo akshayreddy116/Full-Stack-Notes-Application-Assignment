38e122ebb62aa80086f72c10fbb97facae172231
N.Akshay <akshayreddynareddy386@gmail.com>
2026-04-16 10:06:26 +0530

    render link fixed

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,15 @@ const notesRoutes = require("./routes/notes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: "https://full-stack-notes-application-assignment.onrender.com",
+}));
 
+app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/notes", notesRoutes);
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
